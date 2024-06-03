4a5806d94bef8f4f8513a053d4ac69e4789528d4
nijatumuyev <nijatumusoy@gmail.com>
2024-06-03 16:45:24 +0400

    .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Wrap your app with Flexify for initialize
-    return Flexify(
+    return const Flexify(
       designWidth: 375,
       designHeight: 812,
       app: MaterialApp(
